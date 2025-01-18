c095155bb77078da8562bf25fd9e6ed8650bf9fc
Timothy Hays <timothy.hays@bc.edu>
2025-01-18 18:54:51 -0500

    Initial Commit

---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by Tim Hays on 1/18/25.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
