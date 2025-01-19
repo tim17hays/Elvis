7d5dbddfd32883a0d5b8808e5e22bf1f59924b3e
Timothy Hays <timothy.hays@bc.edu>
2025-01-18 19:13:49 -0500

    Chapter 1. Elvis

--- Elvis/ContentView.swift
+++ Elvis/ContentView.swift
@@ -8,12 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var imageName = ""
+    @State private var message = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+            Text("What's so funny 'bout")
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundStyle(.purple)
+            
+            Spacer()
+            Image(systemName: imageName)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 300, height: 300)
+               
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundStyle(.purple)
+    
+            Spacer()
+            
+            HStack {
+                Button("Peace"){
+                    message = "Peace"
+                    imageName = "peacesign"
+                }
+                Button("Love"){
+                    message = "Love"
+                    imageName = "heart"
+                }
+                Button("Understanding"){
+                    message = "Understanding"
+                    imageName = "lightbulb"
+                }
+            }
+                .buttonStyle(.borderedProminent)
+                .font(.title2)
+                .tint(.purple)
+            
         }
         .padding()
     }
